da68b22ce61dc1157bb29006b0091d23e4f6699c
hwk06023 <hwk06023@gmail.com>
2022-11-09 16:31:27 +0900

    Upload 2775

--- Baekjoon/Baekjoon_2775.py
+++ Baekjoon/Baekjoon_2775.py
@@ -1,11 +1,19 @@
 num = int(input())
 
-flag = [[0, 0]] * num
+flag = []
 
-print(flag[0][0])
-'''
-for i in range(0, num):
-    for j in range(2):
-        flag[i][j] = int(input())
-        print(flag)
-'''
+for i in range(num):
+    flag.append([])
+    flag[i].append(int(input()))
+    tmp = int(input())
+    flag[i].append(tmp - 1)
+
+map_li = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]]
+
+for i in range(15):
+    map_li.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    for j in range(1, 14):
+        map_li[i+1][j] = map_li[i][j] + map_li[i+1][j-1]
+
+for i in range(num):
+    print(map_li[flag[i][0]][flag[i][1]])

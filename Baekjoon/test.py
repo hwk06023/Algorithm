783e218514dca664776f11baf28c2a8e9dbfb7d7
hwk06023 <hwk06023@gmail.com>
2023-01-14 19:18:36 +0900

    updaet

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,8 +1,19 @@
-n = int(input())
+n, k = map(int, input().split())
 li = []
+cnt = 0
+i_cnt = 0
 
 for i in range(n):
-    li.append(int(input()))
+    tmp_n = int(input())
     
-for i in range(n):
+    if tmp_n <= k:
+        li.append(tmp_n)
     
+li.sort(reverse=True)
+
+for x in li:
+    if k >= x:
+        cnt += k // x
+        k %= x
+    
+print(cnt)
